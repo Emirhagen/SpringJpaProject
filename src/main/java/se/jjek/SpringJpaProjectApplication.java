de6b8361dd88d9f74420787e84ab863c3ee84dff
Kevin <Kevin@Kevins-MacBook-Pro.local>
2017-02-02 11:28:14 +0100

    first commit

---
+++ src/main/java/se/jjek/SpringJpaProjectApplication.java
@@ -0,0 +1,12 @@
+package se.jjek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaProjectApplication.class, args);
+	}
+}
